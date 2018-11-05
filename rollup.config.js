8a084a498f1193715d2e9332bdc6f531adea3baf
hyejin2351 <hyejin2351@joyfun.kr>
2018-11-05 18:01:04 +0900

    eslint 기능추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
 
 export default {
     input: 'public/js/index.js',
@@ -6,9 +7,13 @@ export default {
         file: 'public/js/bundle.js',
         format: 'iife'
     },
-    puginds: [
+    plugins: [
         babel({
             exclude: 'node_modules/**'   
+        }),
+        eslint({
+            include: './public/js/**',
+            // 이 경로 안에 있는 모든 js 파일의 문법을 검사한다.
         })
-    ]
+    ],
 };
