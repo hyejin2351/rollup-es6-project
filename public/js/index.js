2b1dd4f324f0ae99244352f7ae5a18ae4a65032f
hyejin Kim <hyejin2351@gmail.com>
2018-11-06 13:39:22 +0900

    Sass  플러그인 코드 적용 및 예제 파일 (./public/sass/main.sass) 추가

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 import type from './utils/index';
+import '../sass/main.sass';
 
 const result = type(Symbol('type checker'));
 
-// console.log(result)
+// console.log(result);
--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,33 @@
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 import eslint from 'rollup-plugin-eslint';
+import sass from 'rollup-plugin-sass';
+import autoprefixer from 'autoprefixer';
+import postcss from 'postcss';
 
 // console.log(process.env.BUILD === 'on');
 
 export default {
-  input: 'public/js/index.js',
-  output: {
-    file: 'public/js/bundle.js',
-    format: 'iife',
-  },
-  plugins: [
-    (process.env.BUILD === 'on' && uglify()),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    (process.env.BUILD !== 'on' && eslint({
-      //BUILD 상태가 'on'이 아닌 상태에서만 eslint를 실행
-      include: './public/js/**',
-      exclude: './public/js/bundle.js',
-    })),
-  ],
+    input: 'public/js/index.js',
+    output: {
+        file: 'public/js/bundle.js',
+        format: 'iife',
+    },
+    plugins: [
+        sass({
+            output: './public/css/bundle.css',
+            processor: css => postcss([autoprefixer])
+                .process(css)
+                .then(result => result.css)
+        }),
+        babel({
+            exclude: 'node_modules/**',
+        }),
+        (process.env.BUILD !== 'on' && eslint({
+            //BUILD 상태가 'on'이 아닌 상태에서만 eslint를 실행
+            include: './public/js/**',
+            exclude: './public/js/bundle.js',
+        })),
+        (process.env.BUILD === 'on' && uglify()),
+    ],
 };
